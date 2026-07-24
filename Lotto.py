48d5a1f502d5013603a7ffb80b330ce18b87b814
Zertisx <dropadrian2008@gmail.com>
2026-07-24 22:35:30 +0200

    smoothen code of win_nubm and remake my_numb so its work correctly now

--- Lotto.py
+++ Lotto.py
@@ -2,25 +2,24 @@ import random
 
 def my_numb():
     numb = [] 
-    for _ in range(6):
-        while 0==0:
-            try:
-                given_numb = int(input("Enter a number in range 1-50: "))
-                if given_numb <= 50 and given_numb > 0:
-                    numb.append(given_numb)
-                    break
-                else:
-                    print("Wrong number. Please make sure to type number in range of 1-50. ")
-            except ValueError:
-                print("Number must be integer")
+    while len(numb) < 6:
+        try:
+            given_numb = int(input("Enter your number: "))
+        except ValueError:
+            print("Number must be an integer")
+            continue
+
+        if given_numb < 1 or given_numb > 50:
+            print("Wrong number. Please enter a number in range 1-50.")
+        elif given_numb in numb:
+            print("You already wrote that number. Try something else.")
+        else:
+            numb.append(given_numb)
     return numb
 
 def win_numb():
-    numb = []
-    for _ in range(6):
-        numb.append(random.randint(1,50))
-    return numb
+    return random.sample(range(1, 51), 6)
 
-
+print("Enter 6 diffrent numbers in range of 1-50.")
 print(my_numb())
 print(win_numb())
