b0b2adc4d705b21671eba48b7c0f5a895a241f69
Zertisx <dropadrian2008@gmail.com>
2026-07-29 16:45:59 +0200

    learn sqlite3

---
+++ database.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+conn = sqlite3.connect('lotto.db')
+c = conn.cursor()
+
+c.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL
+    )
+''')
+c.execute('''
+    CREATE TABLE IF NOT EXISTS play (
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER,
+        player_numbers TEXT,
+        winning_numbers TEXT,
+        prize INTEGER,
+        played_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+''')
+conn.commit()
+conn.close()
